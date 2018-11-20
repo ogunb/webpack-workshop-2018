331d84b6cc6fcd52e91c6152ebf5ad06c31517f5
ogunb <babacanogun@gmail.com>
2018-11-20 16:29:50 +0300

    dev-server

---
+++ src/MakeButton.js
@@ -0,0 +1,8 @@
+function MakeButton(label) {
+  const button = document.createElement('button');
+  button.innerText = label;
+
+  return button;
+}
+
+export default MakeButton;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import nav from './nav';
+import MakeButton from './MakeButton';
+
+const newButton = MakeButton('youwhatt');
+
+document.body.appendChild(newButton);
+
 console.log(nav);
